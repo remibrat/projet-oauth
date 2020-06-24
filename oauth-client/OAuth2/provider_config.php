7a07d9e6c4091a1378ada9c08552005d7a108fdb
Taopol BERQUER <tberquer@rampar.com>
2020-06-24 17:30:12 +0200

    Modification du client_secret.

--- oauth-client/OAuth2/provider_config.php
+++ oauth-client/OAuth2/provider_config.php
@@ -21,7 +21,7 @@ return [
 //    ],
     'oauth' => [
         'class_name' => OAuthProvider::class,
-        'client_id' => 'client_5edfd43b0db573',
+        'client_id' => 'client_5edfd43b0db573.88203718',
         'client_secret' => 'e0a6a1f5c55fafd48cbcce2b7279d4029fad76f4',
         'link' => 'http://localhost:7070/auth?response_type=code&client_id={CLIENT_ID}&state={STATE}&scope=email&redirect_uri={LOCAL_URL}/success',
         'state' => 'DEAZFAEF321432DAEAFD3E13223R',
